83b70c0ebf5aad36b0a419e760904016eee633a8
pavans <pavansingaraju@hotmailcom>
2020-03-05 19:01:24 +0530

    Initial Commit

---
+++ src/test/java/gochad/sf/sfpetclinic/SfPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package gochad.sf.sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
